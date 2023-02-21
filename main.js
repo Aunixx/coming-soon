3ecd1a0be56c0dcfd6d33a04337fa9dee341b7d3
TK-DTP-171 <sahil.arwand2000@gmail.com>
2023-02-21 21:18:23 +0500

    first commit

--- main.js
+++ main.js
@@ -1,50 +1,60 @@
 document.addEventListener("DOMContentLoaded", () => {
   let bar = document.getElementById("bar");
   let hiddenTexts = document.getElementsByClassName("hidden");
+  let wrapper = document.querySelector(".text-wrapper");
+
+  setTimeout(() => {
+    gsap.to(bar, { opacity: 1, duration: 2, delay: 3 });
+    gsap.to(wrapper, { opacity: 1, duration: 3 });
+  }, 3000);
+  // setTimeout(
+  //   () => gsap.to(wrapper, { opacity: 1, duration: 2, delay: 3 }),
+  //   3000
+  // );
 
   setOpacity(0);
 
   dragElement(bar);
 
-  function setOpacity(offset){
-      for(let i = 0; i < hiddenTexts.length; i++){
-          console.log(`distance for text ${hiddenTexts[i].innerHTML}`, offset - hiddenTexts[i].offsetLeft)
-          let op = 1 - Math.abs(offset - hiddenTexts[i].offsetLeft) / 500;
-          console.log(op);
-          hiddenTexts[i].style.opacity = op;
-        }
+  function setOpacity(offset) {
+    for (let i = 0; i < hiddenTexts.length; i++) {
+      let op = 1 - Math.abs(offset - hiddenTexts[i].offsetLeft) / 400;
+      console.log(op);
+      hiddenTexts[i].style.opacity = op;
+    }
   }
 
-  function dragElement(elmnt){
-      let dist = 0, currentPos = 0;
+  function dragElement(elmnt) {
+    let dist = 0,
+      currentPos = 0;
 
-      elmnt.onmousedown = dragMouseDown;
+    elmnt.onmousedown = dragMouseDown;
 
-      function dragMouseDown(e){
-          e.preventDefault();
-          currentPos = e.clientX;
+    function dragMouseDown(e) {
+      e.preventDefault();
+      currentPos = e.clientX;
 
-          document.onmouseup = closeDragElement;
-          document.onmousemove = elementDrag;
-      }
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
 
-      function elementDrag(e) {
-          e.preventDefault();
+    function elementDrag(e) {
+      e.preventDefault();
 
-          dist = currentPos - e.clientX;
-          currentPos = e.clientX;
+      dist = currentPos - e.clientX;
+      currentPos = e.clientX;
 
-          let updatedPos = elmnt.offsetLeft - dist;
-          elmnt.style.left = updatedPos + "px";
-          
-          setOpacity(elmnt.offsetLeft);
+      let updatedPos = elmnt.offsetLeft - dist;
+      elmnt.style.left = updatedPos + "px";
 
-          document.documentElement.style.setProperty('--cursorX', elmnt.style.left);
-      }
+      setOpacity(elmnt.offsetLeft);
 
-      function closeDragElement(){
-          document.onmouseup = null;
-          document.onmousemove = null;
-      }
+      document.documentElement.style.setProperty("--cursorX", elmnt.style.left);
+    }
+
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
   }
 });
